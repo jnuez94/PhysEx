9b74ad77b368ef1144612bd0ac05d55689da286a
Justin Pugliese <jmpuglie@us.ibm.com>
2016-11-10 19:20:44 -0500

    added some more but theres a syntax error to work out

--- semantic.ml
+++ semantic.ml
@@ -3,20 +3,23 @@ open Ast
 module StringMap = Map.Make(String)
 
 (*	Semantic checking of a program. Returns void if successful,
-		thows an exception if something is wrong.
+		throws an exception if something is wrong.
 		Check each global variable, then check each function *)
 
-let checker (globals, functions) = function
+let checker (globals, functions) =
 
-	(*	Raise an exception if the given list has a duplicate	*)
+(**	Raise an exception if the given list has a duplicate
+ *----------------------------------------------------------------------------*)
 	let report_duplicate exceptf list =
 		let rec helper = function
-				n1 :: n2 :: _ when n1 = n2 -> raise (Failture (exceptf n1))
+				n1 :: n2 :: _ when n1 = n2 -> raise (Failure (exceptf n1))
 			|	_ :: t -> helper t
 			| [] -> ()
 		in helper (List.sort compare list)
 	in
 
+(**	Helper functions
+ *----------------------------------------------------------------------------*)
 	(*	Raise an exception if a given binding is to a void type	*)
 	let check_not_void exceptf = function
 			(Void, n) -> raise (Failure (exceptf n))
@@ -29,16 +32,43 @@ let checker (globals, functions) = function
 		if lvaluet == rvaluet then lvaluet else raise err
 	in
 
-	(**** Checking global variables ****)
+	(**	Checking global variables **)
 	List.iter (check_not_void (fun n -> "illegal void global " ^ n)) globals;
 
 	report_duplicate (fun n -> "duplicate global " ^ n) (List.map snd globals);
 
-	(**** Checking functions ****)
+	(**	Checking functions **)
 	if List.mem "print" (List.map (fun fd -> fd.fname) functions)
 	then raise (Failure ("function print may not be defined"))
 	else ();
 
 	report_duplicate (fun n -> "duplicate function " ^ n) (List.map (fun fd -> fd.fname) functions);
 
-	(*	Function declaration for a named function	*)
+(**	Print function declaration
+ *----------------------------------------------------------------------------*)
+let built_in_decls = StringMap.add "print"
+	{
+		fname = "print";
+		formals = [];
+		locals = [];
+		body = []
+	} (StringMap.add "xxx" {
+		fname = "xxx";
+		formals = [];
+		locals = [];
+		body = []
+	})
+in
+let function_decls =
+	List.fold_left (fun m fd -> StringMap.add fd.fname fd m)
+		built_in_decls functions
+in
+let function_decl s = try StringMap.find s function_decls
+	with Not_found -> raise (Failure ("unrecognized function " ^ s))
+in
+
+(**	Return the type of an expression or throw an error
+ *----------------------------------------------------------------------------*)
+let rec expr = function
+		NumList _ -> Int
+	| BoolLit _ -> Bool
